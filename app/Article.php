6972b3984d6b54e8622d2eb6ce2cfee178899dce
fm ti <starprefix@gmail.com>
2022-01-10 02:21:32 +0330

    laravel project

---
+++ app/Article.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use Sluggable;
+
+    protected $fillable = ['title' , 'slug' , 'body'];
+
+    /**
+     * Return the sluggable configuration array for this model.
+     *
+     * @return array
+     */
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+}
